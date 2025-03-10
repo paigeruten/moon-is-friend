c5d13da3df7b5e88e0e9cb4c090388d9b38be595
Paige Ruten <paige.ruten@gmail.com>
2025-03-09 23:02:36 -0600

    Launcher icons, more googly eyes

--- src/main.lua
+++ src/main.lua
@@ -300,9 +300,20 @@ function pd.update()
     local animFrame = math.min(frameCount, 700)
 
     -- Earth
+    local earthX, earthY = screenWidth - 60, screenHeight // 4
     gfx.setColor(gfx.kColorWhite)
     gfx.setDitherPattern(0.45, gfx.image.kDitherTypeBayer8x8)
-    gfx.fillCircleAtPoint(screenWidth - 60, screenHeight // 4, 20)
+    gfx.fillCircleAtPoint(earthX, earthY, 20)
+
+    -- Earth eyes
+    local leftEye = pd.geometry.point.new(earthX - 7, earthY - 7)
+    local rightEye = pd.geometry.point.new(earthX + 7, earthY - 7)
+    gfx.setColor(gfx.kColorWhite)
+    gfx.fillCircleAtPoint(leftEye, 5)
+    gfx.fillCircleAtPoint(rightEye, 5)
+    gfx.setColor(gfx.kColorBlack)
+    gfx.fillCircleAtPoint(leftEye, 2)
+    gfx.fillCircleAtPoint(rightEye, 2)
 
     -- Moon
     gfx.setColor(gfx.kColorWhite)
